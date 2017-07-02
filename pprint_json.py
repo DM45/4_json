38f082320351676cd023fb5234b2d5978e9a3be2
DM45 <mda.tomsk@gmail.com>
2017-07-02 10:08:30 +0700

    Выполнение поставленной задачи

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import os
 
 
-def load_data(filepath):
-    pass
+def load_json_data(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    _filepath = input(str)
+    _data = load_json_data(_filepath)
+    print(pretty_print_json(_data))
